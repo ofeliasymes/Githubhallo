11513ba897bdb9d9c547f10994be574adb27c06b
ofeliasymes <36763197+ofeliasymes@users.noreply.github.com>
2018-11-15 10:38:55 +0100

    Commit besked om Hallo World

--- Githubhallo/Program.cs
+++ Githubhallo/Program.cs
@@ -7,6 +7,7 @@ namespace Githubhallo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo World!");
         }
     }
 }
