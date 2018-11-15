c6bd21aacd561e67d5fff4b5b65d032d0f705ea5
ofeliasymes <36763197+ofeliasymes@users.noreply.github.com>
2018-11-15 10:28:09 +0100

    Add project files.

---
+++ Githubhallo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Githubhallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
